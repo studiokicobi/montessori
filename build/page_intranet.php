4739a2546717e71b7ee9bfaf8758acac0dc1cc40
unlikelyobjects <hello@unlikelyobjects.com>
2018-12-10 13:37:42 +0100

    member form

--- build/page_intranet.php
+++ build/page_intranet.php
@@ -27,7 +27,6 @@ if (!is_user_logged_in()) {
 
 <?php // Intranät start page
 if (is_page(856)) {
-  echo 'test';
   if ( ! empty($_GET['user']) ) :
 	   $user_id = ( is_numeric($_GET['user']) ? 'user_' . $_GET['user'] : 'new_user' );
 	    acf_form([
@@ -35,7 +34,6 @@ if (is_page(856)) {
 		        'post_id'      => $user_id,
 	         ]);
   endif;
- endif;
 // Close Intranät start page page
 } ?>
 
