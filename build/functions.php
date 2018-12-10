3c8d885b4139c93645ff485b62a908ea87592b12
unlikelyobjects <hello@unlikelyobjects.com>
2018-12-10 12:21:05 +0100

    Update functions.php

--- build/functions.php
+++ build/functions.php
@@ -401,22 +401,22 @@ function my_acf_save_post($post_id)
  *
  */
 
-// function wpb_list_child_pages()
-//{
-//    global $post;
-//
-//    if (is_page() && $post->post_parent) {
-//        $childpages = wp_list_pages('sort_column=menu_order&title_li=&child_of=' .$post->post_parent . '&echo=0');
-//    } else {
-//        $childpages = wp_list_pages('sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0');
-//    }
-//    if ($childpages) {
-//        $string = '<ul class="subpage_nav">' . $childpages . '</ul>';
-//    }
-//    return $string;
-//}
+function wpb_list_child_pages()
+{
+    global $post;
 
-//add_shortcode('wpb_childpages', 'wpb_list_child_pages');
+    if (is_page() && $post->post_parent) {
+        $childpages = wp_list_pages('sort_column=menu_order&title_li=&child_of=' .$post->post_parent . '&echo=0');
+    } else {
+        $childpages = wp_list_pages('sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0');
+    }
+    if ($childpages) {
+        $string = '<ul class="subpage_nav">' . $childpages . '</ul>';
+    }
+    return $string;
+}
+
+add_shortcode('wpb_childpages', 'wpb_list_child_pages');
 
 
 
