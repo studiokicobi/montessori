cbdb18a6a55e3a09e1c54ab3f3a3533b699e9436
unlikelyobjects <hello@unlikelyobjects.com>
2018-12-13 22:32:51 +0100

    remove menu items from dashboard

--- functions.php
+++ functions.php
@@ -541,3 +541,25 @@ function create_post_type() {
   );
 }
 add_action( 'init', 'create_post_type' );
+
+
+/**
+ * Remove menu items from WordPress dashboard
+ *
+ */
+
+function remove_menus()
+{
+    //remove_menu_page( 'index.php' ); //Dashboard
+    //remove_menu_page( 'upload.php' ); //Media
+    //remove_menu_page( 'edit.php?post_type=page' ); //Pages
+    //remove_menu_page( 'themes.php' ); //Appearance
+    //remove_menu_page( 'plugins.php' ); //Plugins
+    //remove_menu_page( 'users.php' ); //Users
+    //remove_menu_page( 'tools.php' ); //Tools
+    //remove_menu_page( 'edit.php?post_type=portfolio' ); //Portfolio
+    //remove_menu_page( 'options-general.php' ); //Settings
+    remove_menu_page( 'edit.php' ); //Posts
+    remove_menu_page( 'edit-comments.php' ); //Comments
+}
+add_action( 'admin_menu', 'remove_menus' );
